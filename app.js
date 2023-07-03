131fc365cd9b23c4ef2537b334e56ed48dd5e2cf
ShyamPandit3107 <shyambpandit3107@gmail.com>
2023-07-03 10:57:48 +0530

    third commit

---
+++ app.js
@@ -0,0 +1,13 @@
+variables
+const cartBtn = document.querySelector('cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+cost cartTotal = document.querySelector('.cart-total');
+const productsDOM = document.querySelector('.product-center');
+let cart = [];
+class Products{
+    
+}
